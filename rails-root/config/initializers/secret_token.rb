b173d51fadb7e9f20880595ae2d6e34b53c36406
DavidSperling <ongakujin@gmail.com>
2012-08-18 22:24:52 -0700

    Removed rails-root folder

--- rails-root/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RailsRoot::Application.config.secret_token = 'a0a0b78112d6182376c1e384245883d0dd35a3888caab4fc244b6c0e012b27b97e52184cf6f2b1f43ad47c627153a47d91f52deedaf530842ec13eefd529aae5'
