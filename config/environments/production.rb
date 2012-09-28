3ac5a12c04f175456a8f19890a67b8c71343750b
DavidSperling <ongakujin@gmail.com>
2012-09-28 01:00:07 -0400

    Fixed mistake in production.rb where jquery.Jcrop.css was being precompiled twice and jquery.Jcrop.js was not being precompiled at all.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ RailsRoot::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( pics/_form.js jcrop/jquery.Jcrop.css jcrop/jquery.Jcrop.css )
+  config.assets.precompile += %w( pics/_form.js jcrop/jquery.Jcrop.css jcrop/jquery.Jcrop.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
